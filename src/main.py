832038461182d9abc12c788dfb63219716759db0
felipe <felipe.baleche2@gmail.com>
2025-04-06 12:45:59 -0300

    add email parsing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 from graph import Graph
+from utils import parse_email
 import os
 
 
 def main() -> None:
-    pass
+    # Traversing the dataset
+    for root, _, files in os.walk("eron-dataset"):
+        for file in files:
+            path = os.path.join(root, file)
+            sender, receivers = parse_email(path)
+
+            if len(receivers) == 0:
+                continue
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,14 @@
+from email import message_from_string
+
+
+def parse_email(path: str) -> tuple[str, list[str]]:
+    with open(path, "r") as f:
+        email_string = f.read()
+        parsed = message_from_string(email_string)
+        sender = parsed["From"]
+        receiver = parsed["To"]
+
+        if receiver is None:
+            return sender, []
+
+        return sender, receiver.split(",")
